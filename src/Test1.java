7fd65e1be0668947435e63bb38269dc719896f55
Sudhanshu Ranjan <Sudhanshu Ranjan@DESKTOP-BKVN8NT>
2022-02-23 17:26:07 -0800

    First Commit(by Sudhanshu Ranjan)....

---
+++ src/Test1.java
@@ -0,0 +1,20 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		    
+		
+		String hungry="Yes";
+		if(hungry=="Yes")
+		{
+			System.out.println("Eat Something....");
+		}
+		else
+		{
+			System.out.println("Do Work......");
+		}
+		
+		
+	}
+
+}
