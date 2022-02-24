e9af9b5c6b62b42770b21171eb241891e8adfe99
Sudhanshu Ranjan <Sudhanshu Ranjan@DESKTOP-BKVN8NT>
2022-02-23 20:52:04 -0800

    Commiting Test2 in BranchSudh

---
+++ src/Test2.java
@@ -0,0 +1,20 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		    
+		
+		String hungry="No";
+		if(hungry=="Yes")
+		{
+			System.out.println("Eat Something....");
+		}
+		else
+		{
+			System.out.println("Do Work......");
+		}
+		
+		
+	}
+
+}
